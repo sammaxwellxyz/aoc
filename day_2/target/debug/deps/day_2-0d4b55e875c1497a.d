51e77f59bcbfc1f3830e36f3e3e7eaafd1275b41
Sam Maxwell <>
2022-12-02 10:16:33 +0000

    remove target

--- day_2/target/debug/deps/day_2-0d4b55e875c1497a.d
+++
@@ -1,5 +0,0 @@
-/Users/samuelmaxwell/Code/aoc/day_2/target/debug/deps/day_2-0d4b55e875c1497a.rmeta: src/main.rs
-
-/Users/samuelmaxwell/Code/aoc/day_2/target/debug/deps/day_2-0d4b55e875c1497a.d: src/main.rs
-
-src/main.rs:
--- day_2/target/debug/deps/day_2-bf9410bc1e1ed2d1.d
+++
@@ -1,5 +0,0 @@
-/Users/samuelmaxwell/Code/aoc/day_2/target/debug/deps/day_2-bf9410bc1e1ed2d1.rmeta: src/main.rs
-
-/Users/samuelmaxwell/Code/aoc/day_2/target/debug/deps/day_2-bf9410bc1e1ed2d1.d: src/main.rs
-
-src/main.rs:
