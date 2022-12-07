83866abc6323a1caae56786d7849652b5197c71d
Sam Maxwell <>
2022-12-07 13:39:55 +0000

    day 5

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,53 +1,68 @@
 use std::fs;
 
+fn build_stacks(stack: &str) -> Vec<Vec<char>> {
+    stack
+        .split("\n")
+        .fold(Vec::new(), |mut acc, level| {
+            if level.contains(" 1 ") {
+                return acc;
+            }
+            level.chars().collect::<Vec<char>>().chunks(4).enumerate().for_each(|(stack_idx, stack_box)| {
+                if acc.len() <= stack_idx {
+                    if stack_box.contains(&'[') {
+                        acc.push(vec![stack_box[1]]);
+                    } else {
+                        acc.push(vec![]);
+                    }
+                } else {
+                    if stack_box.contains(&'[') {
+                        acc[stack_idx].insert(0, stack_box[1]);
+                    }
+                }
+            });
+            acc
+        })
+}
 
 mod a {
+    use super::*;
+
     pub fn run(input: &str) -> String {
         let mut parts = input.split("\n\n");
-        let mut stacks = parts.next().unwrap()
-            .split("\n")
-            .fold(Vec::new(), |mut acc, level| {
-                if level.contains(" 1 ") {
-                    return acc;
-                }
-                level.chars().iter().chunks(4).enumerate().for_each(|(stack_idx, stack_box)| {
-                    println!("{:?} {:?}", stack_idx, stack_box);
-                    stack_box.filter();
-                    if acc.len() <= stack_idx {
-                        if stack_box.contains(&"[") {
-                            acc.push(vec![stack_box[1]]);
-                        } else {
-                            acc.push(vec![]);
-                        }
-                    } else {
-                        if stack_box.contains(&"[") {
-                            acc[stack_idx].insert(0, stack_box[1]);
-                        }
-                    }
-                });
-                acc
-            });
-        println!("shhaa {:?}", stacks);
+        let mut stacks = build_stacks(parts.next().unwrap());
+       
         parts.next().unwrap().split("\n").for_each(|mm| {
             let mut move_parts = mm.split(" ");
             let move_count = move_parts.nth(1).unwrap().parse::<usize>().unwrap();
             let move_from = move_parts.nth(1).unwrap().parse::<usize>().unwrap() - 1;
             let move_to = move_parts.nth(1).unwrap().parse::<usize>().unwrap() - 1;
             for _ in 0..move_count {
-                println!("{:?} {:?}", stacks,move_from );
                 let move_box = stacks[move_from].pop().unwrap();
                 stacks[move_to].push(move_box);
             }
         });
-        println!("{:?}", stacks);
         return stacks.iter().map(|stack| *stack.last().unwrap()).collect();
     }
 }
 
 mod b {
-    pub fn run(input: &str) -> &str {
-        println!("{:?}", input);
-        return "CMZ"
+    use super::*;
+
+    pub fn run(input: &str) -> String {
+        let mut parts = input.split("\n\n");
+        let mut stacks = build_stacks(parts.next().unwrap());
+
+        parts.next().unwrap().split("\n").for_each(|mm| {
+            let mut move_parts = mm.split(" ");
+            let move_count = move_parts.nth(1).unwrap().parse::<usize>().unwrap();
+            let move_from = move_parts.nth(1).unwrap().parse::<usize>().unwrap() - 1;
+            let move_to = move_parts.nth(1).unwrap().parse::<usize>().unwrap() - 1;
+            let stacke_len = stacks[move_from].len();
+            let move_boxes = stacks[move_from].split_off(stacke_len - move_count);
+            stacks[move_to].extend_from_slice(&move_boxes[..]);
+
+        });
+        return stacks.iter().map(|stack| *stack.last().unwrap()).collect();
     }
 }
 
@@ -68,7 +83,7 @@ mod tests {
     fn test() {
         let input = fs::read_to_string("test.txt").expect("input file no worky");
         assert_eq!(a::run(&input), "CMZ");
-        assert_eq!(b::run(&input), "CMZ");
+        assert_eq!(b::run(&input), "MCD");
     }
 
 }
